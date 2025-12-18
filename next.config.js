d3c7811bb5c7774af059e5a0e629b878dc0ea48e
Redouane Sahari <redouane.sahari@gmail.com>
2025-12-18 12:24:58 +0100

    Configuration pour déploiement GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     domains: [],
   },
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
 }
 
 module.exports = nextConfig
